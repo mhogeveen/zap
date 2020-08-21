a36f7253b803c049a8087cca059f7fc01a955062
Maarten Hogeveen <maarten@Maartens-MacBook-Air.local>
2020-08-21 15:52:36 +0200

    Setup for use of axios & redux-thunk

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
 import reducers from './reducers'
 
@@ -9,7 +10,7 @@ import App from './components/App'
 
 import './sass/styles.scss'
 
-const store = createStore(reducers)
+const store = createStore(reducers, applyMiddleware(thunk))
 
 ReactDOM.render(
    <Provider store={store}>
