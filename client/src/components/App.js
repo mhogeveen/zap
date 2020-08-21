7695baf0e560c119a906ba9f01d4649a505c20d2
Maarten Hogeveen <maarten@Maartens-MacBook-Air.local>
2020-08-21 13:23:38 +0200

    Add react router with custom history object

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Router, Switch, Route } from 'react-router-dom'
+import history from '../history'
+
+import Navbar from './Navbar'
 
 const App = () => {
-   return <div>Zap</div>
+   return (
+      <>
+         <Navbar />
+         <Router history={history}>
+            <Switch>
+               <Route exact path='/'>
+                  <div>App</div>
+               </Route>
+            </Switch>
+         </Router>
+      </>
+   )
 }
 
 export default App
